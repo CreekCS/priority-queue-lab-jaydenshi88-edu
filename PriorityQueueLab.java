278fce1f0fddf1ff5edb2e50a9e45532d074fcbf
jaydeshi88 <jaydenshi415@gmail.com>
2024-11-05 13:18:44 -0700

    done

--- PriorityQueueLab.java
+++ PriorityQueueLab.java
@@ -1,6 +1,25 @@
-// import statements
+import java.util.*;
+import java.io.*;
 
 public class PriorityQueueLab {
-  
-}
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pQueue;
+        Scanner scan = null;
+        pQueue = new PriorityQueue<Integer>();
 
+        try {
+            scan = new Scanner(new File("RandIntegers.txt"));
+
+            while (scan.hasNextInt()) {
+                pQueue.add(scan.nextInt());
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File not found. " + e);
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+
+        System.out.println("PriorityQueue contents: " + pQueue);
+    }
+}
